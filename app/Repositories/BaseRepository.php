df852fa71d0a01f16b97d056ca36abebb74d90b7
MOUSSA-AI-DEV <essalhimoussa@gmail.com>
2026-01-03 22:50:55 +0100

    feat: ajouter baserepository

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -2,13 +2,19 @@
 namespace App\Repositories;
 
 use PDO;
+use App\Config\Database;
 
 abstract class BaseRepository
 {
-    protected PDO $pdo;
+    protected PDO $db;
 
-    public function __construct(PDO $pdo)
-    {
-        $this->pdo = $pdo;
+    public function __construct() {
+        $this->db = Database::conn();
+    }
+
+    protected function q(string $sql, array $params = []) {
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
     }
 }
